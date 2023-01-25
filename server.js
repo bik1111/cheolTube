fc0890f597cac653278fcf08898efe8612b37d4f
bik1111 <bik1111@khu.ac.kr>
2023-01-25 19:11:28 +0900

    upload my app

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
-app.set("views", process.cwd() + "/views");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
 
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/wetube'
+const dbUrl =  'mongodb://localhost:27017/wetube'
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
